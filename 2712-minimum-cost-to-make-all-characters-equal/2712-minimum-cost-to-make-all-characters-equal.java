d2304e3247aeb2012faf3443cf4416f770bbe0a8
Jyoti Singh <98025162+Singhjyoti24@users.noreply.github.com>
2023-07-10 16:43:21 +0530

    Time: 12 ms (70.76%), Space: 44.1 MB (98.87%) - LeetHub

---
+++ 2712-minimum-cost-to-make-all-characters-equal/2712-minimum-cost-to-make-all-characters-equal.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long minimumCost(String s) {
+        long ans = 0;
+        
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i)!=s.charAt(i-1)){
+                ans += Math.min(i, s.length()-i);
+            }
+        }
+        return ans;
+    }
+}
