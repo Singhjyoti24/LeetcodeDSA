ab802effed4b9e0b50d508d95e18195a3c299218
Jyoti Singh <98025162+Singhjyoti24@users.noreply.github.com>
2023-09-13 21:16:36 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -51,42 +51,33 @@ class Solution
     //Function to return max value that can be put in knapsack of capacity W.
     static int[][] dp;
 
-    static int knapSack(int W, int[] wt, int[] val, int n) {
-        // Initialize the memoization table with -1 values
+    static int knapSack(int W, int wt[], int val[], int n) {
         dp = new int[n + 1][W + 1];
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i < dp.length; i++) {
             Arrays.fill(dp[i], -1);
         }
 
-        // Call the recursive helper function
-        return knapSackHelper(W, wt, val, n);
+        return knapSackHelper(W, wt, val, n, dp);
     }
 
-    static int knapSackHelper(int W, int[] wt, int[] val, int n) {
-        // Base case: If either the capacity or the number of items is 0, the value is 0
+    public static int knapSackHelper(int W, int wt[], int[] val, int n, int[][] dp) {
         if (W == 0 || n == 0) {
             return 0;
         }
 
-        // If the result is already computed, return it
         if (dp[n][W] != -1) {
             return dp[n][W];
         }
 
-        // If the weight of the current item is more than the remaining capacity,
-        // it cannot be included in the knapsack
-        if (wt[n - 1] > W) {
-            dp[n][W] = knapSackHelper(W, wt, val, n - 1);
+        if (wt[n - 1] <= W) {
+            dp[n][W] = Math.max(val[n - 1] + knapSackHelper(W - wt[n - 1], wt, val, n - 1, dp),
+                    knapSackHelper(W, wt, val, n - 1, dp));
         } else {
-            // Otherwise, consider two options: include the item or exclude it
-            int include = val[n - 1] + knapSackHelper(W - wt[n - 1], wt, val, n - 1);
-            int exclude = knapSackHelper(W, wt, val, n - 1);
-            dp[n][W] = Math.max(include, exclude);
+            dp[n][W] = knapSackHelper(W, wt, val, n - 1, dp);
         }
 
         return dp[n][W];
     }
-
 }
 
 
